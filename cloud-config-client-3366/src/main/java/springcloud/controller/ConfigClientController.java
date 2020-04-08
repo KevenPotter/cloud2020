4b4f41f06d6aa6cc6d235414f977fa7c14db0311
KevenPotter <keven_potter@163.com>
2020-04-08 17:11:52 +0800

    commit

---
+++ cloud-config-client-3366/src/main/java/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 要想自动获取配置生效,需要运维工作人员手动POST请求通知3355进行配置更改
+// crul -X POST "http://localhost:3355/actuator/refresh"
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo" + configInfo;
+    }
+}
